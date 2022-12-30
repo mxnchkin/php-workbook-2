b754737eaef537c09862a9d0c2969b15c81e8827
mxnchkin <97164758+mxnchkin@users.noreply.github.com>
2022-12-30 03:03:49 +0000

    Create nonsecurepage.php

---
+++ nonsecurepage.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<HTML>
+<head>
+	<title>Test Signon - v0.3</title>
+</head>
+
+<body>
+
+	<h1>Test Signon - v0.3</h1>
+	
+Non secure content goes here
+
+	<form name='form1' id='form1' action="index.html" method="get">
+		Home : <input type="submit" value="Home">
+	</form>
+		
+</body>
+
+</html>
